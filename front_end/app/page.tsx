40482aecaf1186a968ca2fbb403916b4e1e130eb
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-10-23 22:50:23 -0400

    Simplified board adapts to changes in viewport height and width

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -1,51 +1,48 @@
+import React from 'react';
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      {/* Row 1 */}
-      <div className="flex">
-        <HexTile />
-        <HexTile />
-        <HexTile />
-      </div>
-      {/* Row 2 */}
-      <div className="flex" style={{ marginTop: '-24px' }}>
-        <HexTile />
-        <HexTile />
-        <HexTile />
-        <HexTile />
-      </div>
-      {/* Row 3 */}
-      <div className="flex" style={{ marginTop: '-24px' }}>
-        <HexTile />
-        <HexTile />
-        <HexTile />
-        <HexTile />
-        <HexTile />
-      </div>
-      {/* Row 4 */}
-      <div className="flex" style={{ marginTop: '-24px' }}>
-        <HexTile />
-        <HexTile />
-        <HexTile />
-        <HexTile />
-      </div>
-      {/* Row 5 */}
-      <div className="flex" style={{ marginTop: '-24px' }}>
-        <HexTile />
-        <HexTile />
-        <HexTile />
+    <div className="board-container">
+      <div className="board">
+        {/* Row 1 */}
+        <div className="row">
+          <HexTile />
+          <HexTile />
+          <HexTile />
+        </div>
+        {/* Row 2 */}
+        <div className="row offset">
+          <HexTile />
+          <HexTile />
+          <HexTile />
+          <HexTile />
+        </div>
+        {/* Row 3 */}
+        <div className="row">
+          <HexTile />
+          <HexTile />
+          <HexTile />
+          <HexTile />
+          <HexTile />
+        </div>
+        {/* Row 4 */}
+        <div className="row offset">
+          <HexTile />
+          <HexTile />
+          <HexTile />
+          <HexTile />
+        </div>
+        {/* Row 5 */}
+        <div className="row">
+          <HexTile />
+          <HexTile />
+          <HexTile />
+        </div>
       </div>
     </div>
   );
 }
 
 function HexTile() {
-  return (
-    <div
-      className="hex-tile w-24 h-24"
-      style={{ marginLeft: '0px' }}
-    >
-      {/* You can add content inside the hexagon here */}
-    </div>
-  );
+  return <div className="hex-tile"></div>;
 }
