6388b889fb780feb90527b491ad77b18a93a6361
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-10-24 00:14:05 -0400

    Indexed idToColor with an object of type HexID

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
 
+const colorOf = {
+  Brick: 'rgb(170, 74, 68)',
+  Desert: 'rgb(245, 213, 161)',
+  Grain: 'rgb(250, 219, 94)',
+  Ore: 'rgb(128, 128, 128)',
+  Wood: 'rgb(34, 139, 34)',
+  Wool: 'rgb(121, 208, 33)',
+};
+
+const idToColor = {
+  H01: colorOf.Ore,
+  H02: colorOf.Wool,
+  H03: colorOf.Wood,
+  H04: colorOf.Grain,
+  H05: colorOf.Brick,
+  H06: colorOf.Wool,
+  H07: colorOf.Brick,
+  H08: colorOf.Grain,
+  H09: colorOf.Wood,
+  H10: colorOf.Desert,
+  H11: colorOf.Wood,
+  H12: colorOf.Ore,
+  H13: colorOf.Wood,
+  H14: colorOf.Ore,
+  H15: colorOf.Grain,
+  H16: colorOf.Wool,
+  H17: colorOf.Brick,
+  H18: colorOf.Grain,
+  H19: colorOf.Wool,
+} as const;
+
+type HexID = keyof typeof idToColor;
+
 export default function Home() {
 
-  const colorOf = {
-    Brick: 'rgb(170, 74, 68)',
-    Desert: 'rgb(245, 213, 161)',
-    Grain: 'rgb(250, 219, 94)',
-    Ore: 'rgb(128, 128, 128)',
-    Wood: 'rgb(34, 139, 34)',
-    Wool: 'rgb(121, 208, 33)',
-  };
-
-  const idToColor = {
-    H01: colorOf.Ore,
-    H02: colorOf.Wool,
-    H03: colorOf.Wood,
-    H04: colorOf.Grain,
-    H05: colorOf.Brick,
-    H06: colorOf.Wool,
-    H07: colorOf.Brick,
-    H08: colorOf.Grain,
-    H09: colorOf.Wood,
-    H10: colorOf.Desert,
-    H11: colorOf.Wood,
-    H12: colorOf.Ore,
-    H13: colorOf.Wood,
-    H14: colorOf.Ore,
-    H15: colorOf.Grain,
-    H16: colorOf.Wool,
-    H17: colorOf.Brick,
-    H18: colorOf.Grain,
-    H19: colorOf.Wool,
-  };
-
-  const board = [
+  const board: HexID[][] = [
     ['H01', 'H02', 'H03'], // Row 1
     ['H04', 'H05', 'H06', 'H07'], // Row 2
     ['H08', 'H09', 'H10', 'H11', 'H12'], // Row 3
@@ -59,7 +61,7 @@ export default function Home() {
   );
 }
 
-function HexTile({ id, color }: { id: string; color: string }) {
+function HexTile({ id, color }: { id: HexID; color: string }) {
   return (
     <div className="hex-tile" style={{ backgroundColor: color }}>
       {id}
