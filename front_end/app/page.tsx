4b4302f5aeed8512a786087cdbe3882024a11167
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-10-23 23:18:40 -0400

    Numbered hexagons

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -1,48 +1,32 @@
 import React from 'react';
 
 export default function Home() {
+  const board = [
+    [1, 2, 3],          // Row 1
+    [4, 5, 6, 7],       // Row 2 (offset)
+    [8, 9, 10, 11, 12], // Row 3
+    [13, 14, 15, 16],   // Row 4 (offset)
+    [17, 18, 19],       // Row 5
+  ];
+
   return (
     <div className="board-container">
       <div className="board">
-        {/* Row 1 */}
-        <div className="row">
-          <HexTile />
-          <HexTile />
-          <HexTile />
-        </div>
-        {/* Row 2 */}
-        <div className="row offset">
-          <HexTile />
-          <HexTile />
-          <HexTile />
-          <HexTile />
-        </div>
-        {/* Row 3 */}
-        <div className="row">
-          <HexTile />
-          <HexTile />
-          <HexTile />
-          <HexTile />
-          <HexTile />
-        </div>
-        {/* Row 4 */}
-        <div className="row offset">
-          <HexTile />
-          <HexTile />
-          <HexTile />
-          <HexTile />
-        </div>
-        {/* Row 5 */}
-        <div className="row">
-          <HexTile />
-          <HexTile />
-          <HexTile />
-        </div>
+        {board.map((row, rowIndex) => (
+          <div
+            key={rowIndex}
+            className={`row ${rowIndex % 2 === 1 ? 'offset' : ''}`}
+          >
+            {row.map((tileNumber) => (
+              <HexTile key={tileNumber} number={tileNumber} />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-function HexTile() {
-  return <div className="hex-tile"></div>;
+function HexTile({ number }: { number: number }) {
+  return <div className="hex-tile">{number}</div>;
 }
