d243232c890a20baa4fd58a36eaf931e9deed852
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-10-23 22:32:44 -0400

    Removed space between rows of hexagons

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
         <HexTile />
       </div>
       {/* Row 2 */}
-      <div className="flex">
+      <div className="flex" style={{ marginTop: '-24px' }}>
         <HexTile />
         <HexTile />
         <HexTile />
         <HexTile />
       </div>
       {/* Row 3 */}
-      <div className="flex">
+      <div className="flex" style={{ marginTop: '-24px' }}>
         <HexTile />
         <HexTile />
         <HexTile />
@@ -23,14 +23,14 @@ export default function Home() {
         <HexTile />
       </div>
       {/* Row 4 */}
-      <div className="flex">
+      <div className="flex" style={{ marginTop: '-24px' }}>
         <HexTile />
         <HexTile />
         <HexTile />
         <HexTile />
       </div>
       {/* Row 5 */}
-      <div className="flex">
+      <div className="flex" style={{ marginTop: '-24px' }}>
         <HexTile />
         <HexTile />
         <HexTile />
@@ -41,7 +41,10 @@ export default function Home() {
 
 function HexTile() {
   return (
-    <div className="hex-tile w-24 h-24">
+    <div
+      className="hex-tile w-24 h-24"
+      style={{ marginLeft: '0px' }}
+    >
       {/* You can add content inside the hexagon here */}
     </div>
   );
