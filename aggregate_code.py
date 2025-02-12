3925e2047d2c598fea21eed601741b1458a117e6
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-02-12 11:23:16 -0500

    Reorganize

--- aggregate_code.py
+++ aggregate_code.py
@@ -6,31 +6,42 @@ base_path = r'C:\Users\thoma\Documents\Settlers_Of_Catan'
 file_paths = [
     r'generate_board_geometry.py',
     r'board_geometry.json',
+
     r'back_end\__init__.py',
+    r'back_end\app.py',
+    r'back_end\config.py',
+
     r'back_end\ai\__init__.py',
     r'back_end\ai\mcts_node.py',
     r'back_end\ai\neural_network.py',
     r'back_end\ai\self_play.py',
     r'back_end\ai\strategy.py',
-    r'back_end\app.py',
-    r'back_end\config.py',
+    r'back_end\ai\train.py',
+
+    r'back_end\db\__init__.py',
     r'back_end\db\database.py',
+
+    r'back_end\routes\__init__.py',
     r'back_end\routes\next.py',
     r'back_end\routes\roads.py',
     r'back_end\routes\root.py',
     r'back_end\routes\settlements.py',
-    r'back_end\routes\__init__.py',
+
     r'back_end\utilities\__init__.py',
     r'back_end\utilities\board.py',
+
     r'front_end\app\config.ts',
     r'front_end\app\globals.scss',
     r'front_end\app\page.tsx',
+
     r'front_end\app\components\HexTile.tsx',
     r'front_end\app\components\Ocean.tsx',
     r'front_end\app\components\Port.tsx',
     r'front_end\app\components\SettlementMarker.tsx',
     r'front_end\app\components\Vertex.tsx',
+
     r'front_end\app\hooks\useApi.ts',
+
     r'front_end\app\utilities\board.ts'
 ]
 
