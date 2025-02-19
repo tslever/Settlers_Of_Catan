13d23d0168d0fd35d3de249f800c2ee864f1dfdc
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-02-19 16:12:44 -0500

    Cities look more like cities

--- front_end/app/components/CityMarker.tsx
+++ front_end/app/components/CityMarker.tsx
@@ -10,22 +10,44 @@ const CityMarker: React.FC<CityMarkerProps> = ({ x, y, player }) => {
         2: 'orange',
         3: 'green'
     };
+
+    const settlementWidth = 3;
+    const baseWidth = settlementWidth * 2;
+    const baseHeight = settlementWidth;
+
     return (
         <div
             className = "city"
             style = {{
+                position: 'absolute',
                 left: `${x}vmin`,
                 top: `${y}vmin`,
-                position: 'absolute',
-                transform: 'translate(-50%, -50%)',
-                width: '4vmin',
-                height: '4vmin',
-                borderRadius: '50%',
-                border: `2px solid ${colorMapping[player] || 'gray'}`,
-                backgroundColor: 'white',
-                zIndex: 3
+                transform: 'translate(-50%, -100%)',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
             }}
-        />
+        >
+            {/* Settlement part */}
+            <div
+                style = {{
+                    width: `${settlementWidth}vmin`,
+                    height: `${settlementWidth}vmin`,
+                    backgroundColor: colorMapping[player] || 'gray',
+                    clipPath: 'polygon(50% 0%, 100% 40%, 100% 100%, 0% 100%, 0% 40%'
+                }}
+            />
+            {/* Base part */}
+            <div
+                style = {{
+                    marginTop: '0.2vmin',
+                    width: `${baseWidth}vmin`,
+                    height: `${baseHeight}vmin`,
+                    backgroundColor: colorMapping[player] || 'gray',
+                    border: '1px solid black'
+                }}
+            />
+        </div>
     );
 };
 
