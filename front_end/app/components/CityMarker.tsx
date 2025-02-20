fb3b38c2bc52475b50067661572d2f5dbef0b51b
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-02-19 21:51:53 -0500

    Move inline styles to styled components

--- front_end/app/components/CityMarker.tsx
+++ front_end/app/components/CityMarker.tsx
@@ -1,53 +1,47 @@
+import { styled } from 'styled-components';
+
 type CityMarkerProps = {
     x: number;
     y: number;
     player: number;
 };
 
+const colorMapping: { [key: number]: string } = {
+    1: 'red',
+    2: 'orange',
+    3: 'green'
+};
+
+const CityContainer = styled.div`
+    position: absolute;
+    transform: translate(-75%, -50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const SettlementPart = styled.div<{ $bgColor: string }>`
+    width: 3vmin;
+    height: 3vmin;
+    background-color: ${(props) => props.$bgColor};
+    clip-path: polygon(50% 0%, 100% 40%, 100% 100%, 0% 100%, 0% 40%);
+`;
+
+const BasePart = styled.div<{ $bgColor: string }>`
+    margin-top: 0.2vmin;
+    width: 6vmin;
+    height: 3vmin;
+    background-color: ${(props) => props.$bgColor};
+    transform: translate(25%, -10%);
+`;
+
 const CityMarker: React.FC<CityMarkerProps> = ({ x, y, player }) => {
-    const colorMapping: { [key: number]: string } = {
-        1: 'red',
-        2: 'orange',
-        3: 'green'
-    };
-
-    const settlementWidth = 3;
-    const baseWidth = settlementWidth * 2;
-    const baseHeight = settlementWidth;
-
+    const playerColor = colorMapping[player] || 'gray';
     return (
-        <div
-            className = "city"
-            style = {{
-                position: 'absolute',
-                left: `${x}vmin`,
-                top: `${y}vmin`,
-                transform: 'translate(-75%, -50%)',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}
-        >
-            {/* Settlement part */}
-            <div
-                style = {{
-                    width: `${settlementWidth}vmin`,
-                    height: `${settlementWidth}vmin`,
-                    backgroundColor: colorMapping[player] || 'gray',
-                    clipPath: 'polygon(50% 0%, 100% 40%, 100% 100%, 0% 100%, 0% 40%'
-                }}
-            />
-            {/* Base part */}
-            <div
-                style = {{
-                    marginTop: '0.2vmin',
-                    width: `${baseWidth}vmin`,
-                    height: `${baseHeight}vmin`,
-                    backgroundColor: colorMapping[player] || 'gray',
-                    transform: 'translate(25%, -10%)',
-                }}
-            />
-        </div>
+        <CityContainer style = {{ left: `${x}vmin`, top: `${y}vmin` }}>
+            <SettlementPart $bgColor = {playerColor} />
+            <BasePart $bgColor = {playerColor} />
+        </CityContainer>
     );
 };
 
