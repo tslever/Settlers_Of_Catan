5533260b06a1578592d1b5ceb171ad8d232e9677
Tom Lever <46533664+tslever@users.noreply.github.com>
2025-03-09 20:52:52 -0400

    Reorganize

--- aggregate_code.py
+++ aggregate_code.py
@@ -1,91 +1,9 @@
 import os
 import re
+from paths_of_important_files import paths_of_back_end_files
 
 
 base_path = r'C:\Users\thoma\Documents\Settlers_Of_Catan'
-file_paths = [
-    r'.gitignore',
-
-    r'back_end\back_end.sln',
-    r'back_end\back_end\back_end.vcxproj',
-    r'back_end\back_end\back_end.vcxproj.filters',
-    r'back_end\back_end\back_end.vcxproj.user',
-
-    r'back_end\back_end\back_end.cpp',
-
-    r'back_end\back_end\mcts_node.hpp',
-    r'back_end\back_end\neural_network.hpp',
-
-    r'back_end\back_end\database.hpp',
-    r'back_end\back_end\models.hpp',
-
-    r'back_end\back_end\game_state.hpp'
-]
-file_paths = [
-    r'Python_back_end\ai\mcts\__init__.py',
-    r'Python_back_end\ai\mcts\backpropagation.py',
-    r'Python_back_end\ai\mcts\expansion.py',
-    r'Python_back_end\ai\mcts\node.py',
-    r'Python_back_end\ai\mcts\selection.py',
-    r'Python_back_end\ai\mcts\simulation.py',
-
-    r'Python_back_end\ai\__init__.py',
-    r'Python_back_end\ai\continuous_training.py',
-    r'Python_back_end\ai\io_helper.py',
-    r'Python_back_end\ai\neural_network.py',
-    r'Python_back_end\ai\self_play.py',
-    r'Python_back_end\ai\strategy.py',
-    r'Python_back_end\ai\train.py',
-
-    r'Python_back_end\db\__init__.py',
-    r'Python_back_end\db\change_password.py',
-    r'Python_back_end\db\database.py',
-    r'Python_back_end\db\set_up_or_tear_down_game_database.py',
-
-    r'Python_back_end\game\__init__.py',
-    r'Python_back_end\game\game_actions.py',
-    r'Python_back_end\game\game_state.py',
-
-    r'Python_back_end\routes\__init__.py',
-    r'Python_back_end\routes\cities.py',
-    r'Python_back_end\routes\next.py',
-    r'Python_back_end\routes\reset.py',
-    r'Python_back_end\routes\roads.py',
-    r'Python_back_end\routes\root.py',
-    r'Python_back_end\routes\settlements.py',
-
-    r'Python_back_end\__init__.py',
-    r'Python_back_end\app.py',
-    r'Python_back_end\board.py',
-    r'Python_back_end\logger.py',
-    r'Python_back_end\phase_state_machine.py',
-    r'Python_back_end\phase.py',
-    r'Python_back_end\requirements.txt',
-    r'Python_back_end\settings.py',
-
-    r'front_end\app\components\HexTile.tsx',
-    r'front_end\app\components\Marker.tsx',
-    r'front_end\app\components\MarkerComponents.tsx',
-    r'front_end\app\components\Ocean.tsx',
-    r'front_end\app\components\Port.tsx',
-    r'front_end\app\components\QueryBoundary.tsx',
-    r'front_end\app\components\Vertex.tsx',
-
-    r'front_end\app\hooks\useCentralQuery.ts',
-
-    r'front_end\app\api.ts',
-    r'front_end\app\board.ts',
-    r'front_end\app\BoardLayout.tsx',
-    r'front_end\app\CanvasLayer.tsx',
-    r'front_end\app\globals.scss',
-    r'front_end\app\layout.tsx',
-    r'front_end\app\page.tsx',
-    r'front_end\app\Providers.tsx',
-    r'front_end\app\types.ts',
-
-    r'board_geometry.json',
-    r'generate_board_geometry.py'
-]
 
 
 def aggregate_files(base_path, relative_paths):
@@ -111,7 +29,10 @@ def replace_import_groups(content):
 
 
 def main():
-    aggregated_string = aggregate_files(base_path, file_paths)
+    aggregated_string = aggregate_files(
+        base_path,
+        paths_of_back_end_files
+    )
     #aggregated_string = replace_import_groups(aggregated_string)
     output_path = os.path.join(base_path, 'aggregated_contents.txt')
     try:
---
+++ paths_of_important_files.py
@@ -0,0 +1,94 @@
+paths_of_Python_back_end_files = [
+    r'Python_back_end\ai\mcts\__init__.py',
+    r'Python_back_end\ai\mcts\backpropagation.py',
+    r'Python_back_end\ai\mcts\expansion.py',
+    r'Python_back_end\ai\mcts\node.py',
+    r'Python_back_end\ai\mcts\selection.py',
+    r'Python_back_end\ai\mcts\simulation.py',
+
+    r'Python_back_end\ai\__init__.py',
+    r'Python_back_end\ai\continuous_training.py',
+    r'Python_back_end\ai\io_helper.py',
+    r'Python_back_end\ai\neural_network.py',
+    r'Python_back_end\ai\self_play.py',
+    r'Python_back_end\ai\strategy.py',
+    r'Python_back_end\ai\train.py',
+
+    r'Python_back_end\db\__init__.py',
+    r'Python_back_end\db\database.py',
+
+    r'Python_back_end\game\__init__.py',
+    r'Python_back_end\game\game_actions.py',
+    r'Python_back_end\game\game_state.py',
+
+    r'Python_back_end\routes\__init__.py',
+    r'Python_back_end\routes\cities.py',
+    r'Python_back_end\routes\next.py',
+    r'Python_back_end\routes\reset.py',
+    r'Python_back_end\routes\roads.py',
+    r'Python_back_end\routes\root.py',
+    r'Python_back_end\routes\settlements.py',
+
+    r'Python_back_end\__init__.py',
+    r'Python_back_end\app.py',
+    r'Python_back_end\board.py',
+    r'Python_back_end\logger.py',
+    r'Python_back_end\phase_state_machine.py',
+    r'Python_back_end\phase.py',
+    r'Python_back_end\requirements.txt',
+    r'Python_back_end\settings.py'
+]
+
+paths_of_back_end_files = [
+    r'back_end\back_end.cpp',
+
+    r'back_end\back_end.sln',
+    r'back_end\back_end.vcxproj',
+    r'back_end\back_end.vcxproj.filters',
+    r'back_end\back_end.vcxproj.user',
+
+    r'back_end\database.hpp',
+    r'back_end\game_state.hpp',
+    r'back_end\mcts_node.hpp',
+    r'back_end\models.hpp',
+    r'back_end\neural_network.hpp'
+]
+
+paths_of_configuration_files = [
+    r'.gitignore',
+    r'.vscode\Settlers_Of_Catan.code-workspace'
+]
+
+paths_of_files_to_alter_database = [
+    r'change_password.py',
+    r'set_up_or_tear_down_game_database.py'
+]
+
+paths_of_files_to_generate_board_geometry = [
+    r'board_geometry.json',
+    r'generate_board_geometry.py'
+]
+
+paths_of_front_end_files = [
+    r'front_end\app\components\HexTile.tsx',
+    r'front_end\app\components\Marker.tsx',
+    r'front_end\app\components\MarkerComponents.tsx',
+    r'front_end\app\components\Ocean.tsx',
+    r'front_end\app\components\Port.tsx',
+    r'front_end\app\components\QueryBoundary.tsx',
+    r'front_end\app\components\Vertex.tsx',
+
+    r'front_end\app\hooks\useCentralQuery.ts',
+
+    r'front_end\app\api.ts',
+    r'front_end\app\board.ts',
+    r'front_end\app\BoardLayout.tsx',
+    r'front_end\app\CanvasLayer.tsx',
+    r'front_end\app\globals.scss',
+    r'front_end\app\layout.tsx',
+    r'front_end\app\page.tsx',
+    r'front_end\app\Providers.tsx',
+    r'front_end\app\types.ts',
+
+    r'front_end\package.json'
+]
