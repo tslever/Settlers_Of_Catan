8b56cfa3a393795b40cc302029ad855b8284bffd
Tom Lever <Thomas.Lever.Business@Gmail.com>
2025-03-20 11:24:58 -0400

    Clean back end.

--- back_end/db/create_game_database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE game;
-CREATE USER 'administrator'@'localhost'
-IDENTIFIED BY 'settlers_of_catan';
-GRANT ALL PRIVILEGES ON game.*
-TO 'administrator'@'localhost';
-FLUSH PRIVILEGES;
--- back_end/db/delete_game_database.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS game;
-DROP USER 'administrator'@'localhost';
